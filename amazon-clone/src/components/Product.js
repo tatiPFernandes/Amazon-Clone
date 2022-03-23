2608a9595009ccaf5720ee5cd47a0fe49de36895
Tatiane <tatifernandes026@gmail.com>
2022-03-23 17:48:56 +0000

    created a Product component

---
+++ amazon-clone/src/components/Product.js
@@ -0,0 +1,29 @@
+import Image from "next/image"
+import { useState } from "react"
+import {StarIcon} from "@heroicons/react/solid"
+
+function Product({id,title,price, description, category, image}) {
+  const [rating]= useState(4)
+    return (
+    <div>
+        <p>{category}</p>
+
+        <Image src={image} height={200} width={200} objectFit="contain" />
+        <h4>{title}</h4>
+        
+        {/*Creating an arry and filling it with empty value and maping through the 
+        state of rating} */}
+        <div className="flex">
+            {Array(rating).fill().map(i=>(
+             <StarIcon  className="h-5"/>
+
+            ))}
+
+        </div>
+
+        
+    </div>
+  )
+}
+
+export default Product
--- amazon-clone/src/components/ProductFeed.js
+++ amazon-clone/src/components/ProductFeed.js
@@ -1,12 +1,25 @@
-export function ProductFeed({ products }) {
+import Product from "./Product";
+
+export function ProductFeed({ products  }) {
   
     return (
       <div>
           <p>Products</p>
-          {products.map((product) =>(
-              <p>
-                  {product.title}
-              </p>
+          {products.map(({id,title,price, description, category, image}) =>(
+              
+              //rendering products from product component through props
+              <Product 
+              key={id}
+              id={id}
+              title={title}
+              price={price}
+              description={description}
+              category={category}
+              image={image}
+              
+              
+              />
+              
           ))}
           
           </div>
