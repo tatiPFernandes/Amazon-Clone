f1e5f527879f20aa1363435596454d9b3fcbebf7
Tatiane <tatifernandes026@gmail.com>
2022-03-24 16:05:46 +0000

    built

--- amazon-clone/src/components/ProductFeed.js
+++ amazon-clone/src/components/ProductFeed.js
@@ -4,7 +4,7 @@ export function ProductFeed({ products  }) {
   
     return (
      
-               /* grid-flow-row-dense makes use all the space of the gri  */
+               /* grid-flow-row-dense makes use all the space of the grid  */
       <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto">
           
         {  /*slicing the products to show 4 products before the second banner */}
