0d25014ca1669f90b19d7dd0ed5989aeec3637ea
Tati Fernandes <81470166+tatiPFernandes@users.noreply.github.com>
2022-05-02 18:20:04 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -55,7 +55,7 @@ import {MenuIcon ,SearchIcon, ShoppingCartIcon} from "@heroicons/react/outline"
                       All
                   </p>
                   <p className="link">Prime Video</p>
-                  <p className="link">Amazon Busness</p>
+                  <p className="link">Amazon Business</p>
                   <p className="link">Today's Deal</p>
                   <p className="link hidden lg:inline-flex">Eletronics</p>
                   <p className="link hidden lg:inline-flex">Food & Grocery</p>
