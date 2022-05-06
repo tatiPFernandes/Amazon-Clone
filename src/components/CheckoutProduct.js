3d1bb342f69e133fcc49a1c49c509bdefb4e013d
V H <81470166+tatiPFernandes@users.noreply.github.com>
2022-05-06 18:33:13 +0100

    Remove from Basket implemented

--- src/components/CheckoutProduct.js
+++ src/components/CheckoutProduct.js
@@ -1,22 +1,86 @@
+import { StarIcon } from "@heroicons/react/outline";
+import Currency from"react-currency-formatter"
 import Image from "next/image";
+import { useDispatch } from "react-redux";
+import { addToBasket, removeFromBasket } from "../slices/basketSlice";
 const CheckoutProduct =({
     key,
     id,
     title,
     price,
+    rating,
     description,
     category,
     image,
     hasPrime,
 })=>{
+    const dispach = useDispatch();
+    const addItemToBasket =()=>{
+        const product ={
+    key,
+    id,
+    title,
+    price,
+    rating,
+    description,
+    category,
+    image,
+    hasPrime, 
+        }
+    
+    // Push item into redux
+    dispach(addToBasket(product))
+    };
 
+    const removeItemFromBasket = ()=>{
+    // Remove item from redux
+    dispach(removeFromBasket({id}))
+    }
 
 
     return(
         <div className="grid grid-cols-5">
             <Image src={image} height={200} width={200} objectFit="contain" />
 
-        </div>
+            {/* Middle Section*/}
+
+            <div className="col-span-3 max-5"  >
+                <p>{title}</p>                
+          
+
+            <div className="flex">
+                {Array(rating).fill().map((_,i)=>(
+                    <StarIcon key={i} className="h-5 text-yellow-500"/>
+                ))}
+            </div>
+
+            <p className="text-xs my-2 line-clamp-3" >{description}</p>
+            <Currency quantity={price} currency="GBP"/>
+
+            {hasPrime && (
+                <div className="flex items-center space-x-2" >
+                    <img 
+                    loading="lazy"
+                    className="w-12" 
+                    src="https://links.papareact.com/fdw" alt="prime-logo"
+                    />
+
+                    <p className="text-sm text-gray-500" >FREE Next-day Delivery</p>
+                </div>
+                
+                
+            )} 
+            </div>
+            
+            {/* Add/Remove button */}
+
+                <div className="flex flex-col space-y-2 my-auto justify-self-end ">
+                 <button className="button" onClick={addItemToBasket}>Add to Basket</button> 
+                 <button className="button" onClick={removeItemFromBasket}>Remove from Basket</button>
+                </div>            
+
+            </div>
+        
     )
 }
 
--- src/slices/basketSlice.js
+++ src/slices/basketSlice.js
@@ -10,9 +10,23 @@ export const basketSlice = createSlice({
   reducers: {
     //Actions
     addToBasket: (state, action) => {
-      state.items = [...state.items, action.payload]
+      state.items = [...state.items, action.payload];
     },
-    removeFromBasket: (state, action) => {},
+    //
+    removeFromBasket: (state, action) => {
+      const index = state.items.findIndex((basketItem) => basketItem.id === action.payload.id);
+      let newBasket =[...state.items];
+
+      if (index >= 0){
+        //The item exist in the basket... remove it...
+        newBasket.splice(index, 1);
+      }else{
+        console.warn(`Can not remove product(id: ${action.payload}) as its not in the Basket`
+        );
+      }
+      state.items = newBasket;
+    },
+    
   },
 });
 
